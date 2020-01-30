0ab99127c995bb2d0e7f9aaa0d30348a092098aa
MinexAutomation <minex.automation@gmail.com>
2020-01-30 01:13:13 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Canterbury.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Canterbury.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="CDriveNameConvention"/> implementation of <see cref="ICDriveNameConvention"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddCDriveNameConvention(this IServiceCollection services)
+        {
+            services.AddSingleton<ICDriveNameConvention, CDriveNameConvention>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="CDriveNameConvention"/> implementation of <see cref="ICDriveNameConvention"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<ICDriveNameConvention> AddCDriveNameConventionAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ICDriveNameConvention>(() => services.AddCDriveNameConvention());
+            return serviceAction;
+        }
+    }
+}
